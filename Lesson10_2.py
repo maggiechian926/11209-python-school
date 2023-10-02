e0aa2115c04fa088759badcb1be2bdf0a35bb504
maggiechian926 <maggiechiang.offcial@gmail.com>
2023-10-02 21:33:02 +0800

    modify

--- Lesson10_2.py
+++ Lesson10_2.py
@@ -1,18 +1,28 @@
 class Person:
     def __init__(self,name:str,weight:int,height:int):
-        self.name = name
+        self.__name = name
         self.weight = weight
         self.height = height
+    #property
+    @property
+    def name(self) -> str:
+        return self.__name
+    
+    @property
+    def getBMI(self) -> str:
+        return self.bmi()
+
 
     #method
     def bmi(self) -> float:
         return round(self.weight / (self.height / 100) ** 2,ndigits=2)
 
     def __str__(self) -> str:
-        return f"name={self.name}\nweight={self.weight}\nheight={self.height}"
+        return f"name={self.__name}\nweight={self.weight}\nheight={self.height}"
     
 
 if __name__ == '__main__':
     p1 = Person("robert",78,183)
-    print(p1)
-    print(p1.bmi())
+    #p1.name = "vivan"
+    print(p1.name)    
+    print(p1.getBMI)
