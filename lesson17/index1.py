c2422c3eefaea0cfae5ebeb0d63a475f750f0461
maggiechian926 <maggiechiang.offcial@gmail.com>
2023-10-12 14:44:02 +0800

    modify

--- lesson17/index1.py
+++ lesson17/index1.py
@@ -2,15 +2,27 @@ import dataSource
 import tkinter as tk
 from tkinter import ttk
 
-class Window (tk.Tk):
-    def __init__(self, **kwargs):
-      super().__init__(**kwargs)
-      self.title("這是我的首頁")
-      label= tk.Label(self,text="hello,tkinter!",font= ('Halvetica','35'))
-      label.pack(padx=100,pady=50)
-    
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.title("鄉鎮人口統計")
+        self.configure(background='#A8D8B9')
+
+        topFrame = tk.Frame(self,background='#90B44B')
+        label = ttk.Label(topFrame,text="鄉鎮人口統計",font=('Helvetica', '30'))
+        label.pack(padx=20,pady=20)
+        topFrame.pack()
+
+        bottomFrame = tk.Frame(self,background='#DAC9A6')
+        choices = dataSource.cityNames()
+        choicesvar = tk.StringVar(value=choices)
+        listbox = tk.Listbox(bottomFrame,listvariable=choicesvar,width=13)
+        listbox.pack(pady=20)
+        bottomFrame.pack(expand=True,fill='x')
+
+
 def main():
-    window = Window()
+    window = Window()    
     window.mainloop()
     
 
