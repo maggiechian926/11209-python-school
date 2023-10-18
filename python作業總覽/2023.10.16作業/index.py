74e522c6af3ea5409031ad7a8329f066baf315b9
maggiechian926 <maggiechiang.offcial@gmail.com>
2023-10-18 08:58:23 +0800

    modify

--- python作業總覽/2023.10.16作業/index.py
+++ python作業總覽/2023.10.16作業/index.py
@@ -11,35 +11,25 @@ class Window(tk.Tk):
 class Getstock(Dialog):
     
     def body(self,master):
+        
+        super().__init__(master)
         self.title("個股資料")
          
-        tk.Label(master,text='日期').grid(row=0,sticky=tk.W)
-        tk.Label(master,text='開盤價').grid(row=1, sticky=tk.W)
-        tk.Label(master,text='最高價').grid(row=2, sticky=tk.W)
-        tk.Label(master,text='最低價').grid(row=3, sticky=tk.W)
-        tk.Label(master,text='收市價').grid(row=4, sticky=tk.W)
-        tk.Label(master,text='收盤價').grid(row=5, sticky=tk.W)
-        tk.Label(master,text='成交量').grid(row=6, sticky=tk.W)
+        tk.Label(master,text='日期').grid(row=0,column=1,sticky=tk.W)
+        tk.Label(master,text='開盤價').grid(row=1,column=1, sticky=tk.W)
+        tk.Label(master,text='最高價').grid(row=2,column=1, sticky=tk.W)
+        tk.Label(master,text='最低價').grid(row=3,column=1, sticky=tk.W)
+        tk.Label(master,text='收市價').grid(row=4,column=1, sticky=tk.W)
+        tk.Label(master,text='收盤價').grid(row=5,column=1, sticky=tk.W)
+        tk.Label(master,text='成交量').grid(row=6,column=1, sticky=tk.W)
         
-        self.date = tk.Label(master, width=16)
-        self.open = tk.Label(master, width=16)
-        self.high = tk.Label(master, width=16)
-        self.low = tk.Label(master, width=16)
-        self.close = tk.Label(master, width=16)
-        self.adjclose = tk.Label(master, width=16)
-        self.volume = tk.Label(master, width=16)
-
-
-        self.date.grid(row=0, column=1, sticky=tk.W)
-        self.open.grid(row=1, column=1, sticky=tk.W)
-        self.high.grid(row=2, column=1, sticky=tk.W)
-        self.low.grid(row=0, column=1, sticky=tk.W)
-        self.close.grid(row=1, column=1, sticky=tk.W)
-        self.adjclose.grid(row=2, column=1, sticky=tk.W)
-        self.volume.grid(row=2, column=1, sticky=tk.W)
-        
-        
-        
+        self.date= tk.Label(master, width=16, show='*', sticky=tk.E)
+        self.opan = tk.Label(master, width=16, show='*', sticky=tk.E)
+        self.high = tk.Label(master, width=16, show='*',sticky=tk.E)
+        self.low= tk.Label(master, width=16, show='*',sticky=tk.E)
+        self.colse = tk.Label(master, width=16, show='*',sticky=tk.E)
+        self.adjcolse = tk.Label(master, width=16, show='*',sticky=tk.E)
+        self.volume = tk.Label(master, width=16, show='*',sticky=tk.E)
         return self.date
         
     
@@ -48,6 +38,8 @@ class MyFrame(tk.LabelFrame):
     def __init__(self,master,title,**kwargs):
         super().__init__(master,text=title,**kwargs)
         self.pack(expand=1,fill='both',padx=10,pady=10)
+        scrollbar = ttk.Scrollbar(self, orient='vertical')
+        scrollbar.pack(side='right', fill='y')
         
         self.field =ttk.Treeview(self,columns=['#1','#2','#3','#4','#5','#6','#7'],show="headings")
         
@@ -75,7 +67,7 @@ class MyFrame(tk.LabelFrame):
         item_id = self.field.selection()[0]
         item_dict = self.field.item(item_id)
         print(item_dict['values'])
-        dialog = Getstock(self)
+        dialog = Getstock(self,)
         
       
         
