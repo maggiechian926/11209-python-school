432fde4801aa15b57a9ade6023e4254a68cd49ca
maggiechian926 <maggiechiang.offcial@gmail.com>
2023-11-16 14:09:34 +0800

    modify

--- youbike_render/index.py
+++ youbike_render/index.py
@@ -8,6 +8,7 @@ from youbikeTreeView import YoubikeTreeView
 from threading import Timer
 
 
+
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
@@ -17,6 +18,15 @@ class Window(tk.Tk):
         tk.Label(topFrame,text="台北市youbike及時資料",font=("arial", 20), bg="#333333", fg='#ffffff',padx=10,pady=10).pack(padx=20,pady=20)
         topFrame.pack(pady=30)
         #---------------------------------------
+
+        #----------建立搜尋------------------------
+        middleFrame = ttk.LabelFrame(self,text='')
+        tk.Label(middleFrame,text='站點名稱搜尋:').pack(side='left')
+        search_entry = tk.Entry(middleFrame)
+        search_entry.bind("<KeyRelease>", self.OnEntryClick)
+        search_entry.pack(side='left')        
+        middleFrame.pack(fill='x',padx=20)
+        #----------------------------------------
 
         #---------------建立treeView---------------
         bottomFrame = tk.Frame(self)
@@ -31,13 +41,23 @@ class Window(tk.Tk):
         bottomFrame.pack(pady=(0,30),padx=20)
         #-------------------------------------------
 
+    def OnEntryClick(self,event):
+        searchEntry = event.widget
+        #使用者輸入的文字
+        input_word = searchEntry.get()
+        if input_word == "":
+            lastest_data = datasource.lastest_datetime_data()
+            self.youbikeTreeView.update_content(lastest_data)
+        else:
+            search_data = datasource.search_sitename(word=input_word)
+            self.youbikeTreeView.update_content(search_data)
+
 
 
 def main():
     def update_data(w:Window)->None:
         #-----------更新treeView資料---------------
-        #-----------必需先顯示treeView資料,再更新資料,因為更新資料的時間太長----------
-        
+        global t      
         try:
             datasource.updata_render_data()
             #pass
@@ -46,27 +66,41 @@ def main():
             #window.destroy()
 
         lastest_data = datasource.lastest_datetime_data()
-        w.youbikeTreeView.update_content(lastest_data)
-        
-        
+        try:
+            w.youbikeTreeView.update_content(lastest_data)
+        except RuntimeError: #次執行中止會產生RuntimeError的錯誤
+            return
 
         #w.after(5*60*1000,update_data,w) #每隔5分鐘
         t = Timer(5*60, update_data,args=(window,))
+        
         t.start()
 
+    global t,window
     window = Window()
     window.title('台北市youbike2.0')
     #window.geometry('600x300')
     window.resizable(width=False,height=False)
+    window.protocol("WM_DELETE_WINDOW", on_closing)
     lastest_data = datasource.lastest_datetime_data()
     window.youbikeTreeView.update_content(lastest_data)
     #window.after(1000,update_data,window) 
     t = Timer(1, update_data,args=(window,))
+    print(id(t))
     t.start()         
     window.mainloop()
+
+def on_closing():
+    datasource.threadRun = False #結束次執行緒執行
+    window.destroy()    
+    t.cancel()
+    
     
 
     
 
 if __name__ == "__main__":
-    main()
+    t = None
+    window = None
+    main()    
+    
