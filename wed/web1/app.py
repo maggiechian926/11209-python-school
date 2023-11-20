d6db53d3c25fc19dccbcda2da3b7ef7dad9a7476
maggiechian926 <maggiechiang.offcial@gmail.com>
2023-11-20 06:46:54 +0000

    modify

--- wed/web1/app.py
+++ wed/web1/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask,url_for,render_template
+import random
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    name = "徐國堂"
+    dataFrame = get_dataFrame()
+    return render_template('index.html',name=name,dataFrame=dataFrame)
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+def get_dataFrame()->pd.DataFrame:
+    data = [['徐國堂',67,92,85],
+            ['王XX',78,97,65],
+            ['李XX',92, 79, 85]]    
+    
+    return pd.DataFrame(data,columns=["姓名","國文","英文","數學"])
