d78535638f589e4f0632c888a210cd3735b1bd06
maggiechiang <maggiechiang.offcial@gmail.com>
2023-12-06 08:31:59 +0000

    整理資料夾

--- flask_dash/dash_file/dash_app2.py
+++ flask_dash/dash_file/dash_app2.py
@@ -1,15 +1,36 @@
-from dash import Dash, html
+from dash import Dash, html,dash_table
 import pandas as pd
 import dash_bootstrap_components as dbc
+from . import datasource
+import pandas as pd
 
 dash2 = Dash(requests_pathname_prefix="/dash/app2/",external_stylesheets=[dbc.themes.BOOTSTRAP])
+dash2.title = "台北市youbike及時資料"
+lastest_data = datasource.lastest_datetime_data()
+lastest_df = pd.DataFrame(lastest_data,columns=['站點名稱','更新時間','行政區','地址','總數','可借','可還'])
 
 dash2.layout = html.Div(
-    [html.H1("BootStrap Layout"),
-     html.P("這是段落1"),
-     html.P("這是段落2")
+    [
+        dbc.Container([
+            html.Div([
+                html.Div([
+                    html.H1("台北市youbike及時資料")
+                ],className="col text-center")
+            ],
+            className="row",
+            style={"paddingTop":'2rem'}),
+            html.Div([
+                html.Div([
+                    dash_table.DataTable(
+                        data=lastest_df.to_dict('records'),
+                        columns=[{'id':column,'name':column} for column in lastest_df.columns],
+                        page_size=20
+                    ),
+                ],className="col text-center")
+            ],
+            className="row",
+            style={"paddingTop":'2rem'}),
+        ])
     ],
-    className="container-lg",
-    style={'backgroundColor':'#666'}
-
+    className="container-lg"
     )
