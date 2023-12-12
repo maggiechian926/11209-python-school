0e6780b0c71558ea3df0479df1fc695fba6f0dca
maggiechiang <maggiechiang.offcial@gmail.com>
2023-12-12 02:54:04 +0000

    modify

--- flask_wed/app/route.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-
-def hello_world():
-    return "Hello, MVC框架!"
-
-def index():
-    title = "Jinja 示範"
-    big_word = "SHOW ME JINJA"
-    return render_template('index.html', title=title, big_word=big_word) 
--- flask_wed/myweb/main.py
+++ flask_wed/myweb/main.py
@@ -1,14 +1,7 @@
 from flask import Flask
-from app import create_App
-import sys 
-
-print(sys.path)
-
-
+from app import create_app
 
 app = create_app()
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
